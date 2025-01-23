316afe5294f3752ad2b623861ed1d8243c698e5f
shivamgoyal26 <officialshivamgoyal@gmail.com>
2025-01-23 18:50:06 +0530

    libs installed

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -2,3 +2,9 @@
 
 import React from "react";
 import StoreProvider from "@/state/redux";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <StoreProvider>{children}</StoreProvider>;
+};
+
+export default Providers;
